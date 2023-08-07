77097ace3da7f978c689e6cea1381846ae5f03bb
Dong205 <136000020+Dong205@users.noreply.github.com>
2023-08-07 13:26:16 +0700

    Nộp bài tập buổi 16

---
+++ Day16/ex2/ex02.js
@@ -0,0 +1,2 @@
+var sum = 10 + 20 + 10 / 2
+console.log(sum)
---
+++ Day16/ex3/ex03.js
@@ -0,0 +1,9 @@
+var a = 10, b = 15, c =19, max;
+max = a;
+if (max<b) {
+    max = b;
+}
+if (max<c) {
+    max = c;
+} 
+document.write(`số lớn nhất là ${max}`)
