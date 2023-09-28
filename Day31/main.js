f58b1fb5677035f24b20e67b831cd08c219e5059
Dong205 <136000020+Dong205@users.noreply.github.com>
2023-09-28 12:52:52 +0700

    nộp bài tập buổi 31

---
+++ Day31/main.js
@@ -0,0 +1,44 @@
+var btn = document.querySelector(".btn");
+var countDown = document.querySelector(".count-down");
+function countdown(seconds) {
+    var start = performance.now();
+    var elapsed = 0;
+    var remaining = seconds;
+    var requestId;
+  
+    function handleFrame(timestamp) {
+      elapsed = Math.floor((timestamp - start) / 1000);
+      remaining = seconds - elapsed;
+  
+      if (remaining >= 0) {
+        countDown.innerText = `${remaining}`;
+        if (remaining > 0) {
+          requestId = requestAnimationFrame(handleFrame);
+        }
+        if(remaining === 0) {
+            btn.style.cursor = "pointer";
+            btn.classList.remove("hidden");
+            
+            btn.addEventListener("click", function(e) {
+              window.location = "https://fullstack.edu.vn/";
+            });
+  
+        }
+      }
+    }
+  
+    function update() {
+      if (document.visibilityState === 'visible') {
+        start = performance.now() - (elapsed * 1000); 
+        requestId = requestAnimationFrame(handleFrame);
+      } else {
+        cancelAnimationFrame(requestId); 
+      }
+    }
+  
+    update();
+    document.addEventListener('visibilitychange', update);
+  }
+  
+countdown(30); 
+  
