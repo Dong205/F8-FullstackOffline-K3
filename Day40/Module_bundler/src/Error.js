3b0fb69d5c0f25bf5fba3f43f1885e3113a97824
Dong205 <136000020+Dong205@users.noreply.github.com>
2023-10-30 15:02:27 +0700

    nộp bài tập buổi 40

---
+++ Day40/Module_bundler/src/Error.js
@@ -0,0 +1,3 @@
+export const Error = () => {
+    return `<h1 class="error">404 - Not Found</h1>`;
+};
---
+++ Day40/Module_bundler/src/Utils/Router.js
@@ -0,0 +1,39 @@
+import Navigo from "navigo";
+
+const app = document.querySelector("#App");
+const routerPage = new Navigo("/", { linksSelector: "a" });
+
+const renderRouter = (app, html) => {
+  app.innerHTML = html;
+};
+
+window.navigate = (path) => routerPage.navigate(path);
+
+const renderHtml = (defaultLayout, componentPath, params) => {
+  const layout = defaultLayout() || "";
+  return layout.replace(/\{.*\}/g, componentPath(params));
+};
+
+const configureRoute = (path, component, defaultLayout) => {
+  routerPage.on(path, (params) => {
+    const html = renderHtml(defaultLayout, component, params);
+    renderRouter(app, html);
+  });
+};
+
+const setNotFoundPage = () => {
+  routerPage.notFound(() => {
+    const html = Error();
+    renderRouter(app, html);
+  });
+};
+
+const startRouter = (routes, defaultLayout) => {
+  routes.forEach((route) => {
+    configureRoute(route.path, route.component, defaultLayout);
+  });
+  setNotFoundPage();
+  routerPage.resolve();
+};
+
+export { routerPage, startRouter };
